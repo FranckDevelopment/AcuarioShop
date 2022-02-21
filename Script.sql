374cd2573ee1197114ea3a329e32a762b021a7ab
FranckDeve <fmerinozavaleta@gmail.com>
2022-02-21 03:33:49 -0600

    first2 commit

--- Script.sql
+++ Script.sql
@@ -49,7 +49,9 @@ CREATE TABLE Modelo(
     Id_Modelo INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     Id_Marca INT NOT NULL,
     Modelo VARCHAR(35) NOT NULL,
-    Año INT NOT NULL,
+    Años VARCHAR(10) NOT NULL,
+    CIL INT NOT NULL,
+    TLS FLOAT NOT NULL,
     Motor VARCHAR(50)
 );
 
@@ -61,10 +63,12 @@ CREATE TABLE Categoria(
 
 CREATE TABLE Productos(
     Id_Producto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    Id_Vehiculo INT,
     Id_Cat INT NOT NULL,
-    Nombre VARCHAR(50) NOT NULL,
-    Descripcion VARCHAR(100) NOT NULL,
-    URL_Foto VARCHAR(200) NOT NULL,
+    Nombre VARCHAR(500) NOT NULL,
+    Descripcion VARCHAR(1000) NOT NULL,
+    URL_Foto VARCHAR(900) NOT NULL,
+    Unidades INT NOT NULL,
     Precio FLOAT NOT NULL,
     Id_status INT
 );
@@ -96,6 +100,7 @@ alter table Usuario add foreign key (Id_status) references Estado (Id_status);
 alter table Usuario add foreign key (Id_Direccion) references Direccion (Id_Direccion);
 alter table Productos add foreign key (Id_status) references Estado (Id_status);
 alter table Productos add foreign key (Id_Cat) references Categoria (Id_Cat);
+alter table Productos add foreign key (Id_Vehiculo) references Vehiculo (Id_Vehiculo);
 alter table Categoria add foreign key (Id_status) references Estado (Id_status);
 alter table R_U_V add foreign key (RFC) references Usuario (RFC);
 alter table R_U_V add foreign key (Id_Vehiculo) references Vehiculo (Id_Vehiculo);
@@ -115,6 +120,7 @@ insert into Estado(Status) Values('Registrado');
 insert into Estado(Status) Values('Bloqueado');
 insert into Estado(Status) Values('Activo');
 insert into Estado(Status) Values('Inactivo');
+
 
 
 insert into Categoria(Nom_Cat) values('Bujias');
@@ -145,28 +151,79 @@ insert into Marca(Marca) values ('HYUNDAI');
 insert into Marca(Marca) values ('NISSAN');
 
 
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2011,"TSI 1.4 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2012,"TSI 1.8 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2013,"TSI 1.6 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2014,"TSI 1.4 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2015,"TSI 1.8 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2016,"TSI 1.2 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2017,"TSI 1.4 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2018,"TSI 1.8 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2019,"TSI 1.2 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A1',2020,"TSI 1.2 4CIL");
 ###################################################################################
-
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2011,"TSI 2.0 6CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2012,"TSI 2.0 6CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2013,"TSI 1.8 6CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2014,"TSI 1.6 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2015,"TSI 1.6 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2016,"TSI 1.4 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2017,"TSI 1.4 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2018,"TSI 1.2 4CIL");
-insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A3',2019,"TSI 1.2 4CIL");
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A1', '2011-2015', '4', '1.4', 'CAVG,GHTG');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A1', '2016-2018', '4', '1.4', 'CZCA');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A1', '2016-2018', '4', '1.8', '---');
+###################################################################################
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A3', '2014-2019', '4', '1.4', 'CZEA');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A3', '2008-2013', '4', '1.4', 'CAXC');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A3', '2002-2004', '4', '1.8', 'AUQ');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A3', '2005-2013', '4', '2.0', 'AXX,CCZA');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A3', '2004-2008', '6', '3.2', 'BMJ,BUB');
 ####################################################################################
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A4', '2009-2016', '4', '1.8', 'CDHB,CJEB');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A4', '2002-2008', '4', '1.8', 'BEX,BFB');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A4', '2013-2018', '4', '2.0', 'CNCD');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A4', '1998-2001', '6', '2.8', 'AMX,APR,AQD');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A4', '2005-2007', '6', '3.2', 'AUK');
+INSERT INTO Modelo(Id_Marca,Modelo,Años,CIL,TLS,Motor) VALUES ('1', 'A4', '2004-2008', '8', '4.2', 'BBK');
+####################################################################################
+
+
+
+
+
+
+
+
+
+
+INSERT INTO Usuario(RFC,Nombre,Apellidos,Email,Password,Tel,Id_Direccion,Id_status) VALUES ('MFZA1321E1245', 'Francisco ', 'Merino Zavaleta', 'utp0139968@alumno.utpuebla.edu.mx', 'platanito12', '2224727586', NULL, '2');
+INSERT INTO Direccion(Pais,Estado,Ciudad,Calle,C_P,N_I,N_E) VALUES ('Mexico', 'Puebla', 'Puebla', 'Priv.Hidalgo', '72498', NULL, '7');
+UPDATE Usuario SET Id_Direccion = '1' WHERE Usuario.RFC = 'MFZA1321E1245';
+
+
+
+INSERT INTO Vehiculo(Id_Marca,Id_Modelo) VALUES('1', '1'),('1', '2');
+
+INSERT INTO R_U_V(RFC,Id_Vehiculo) VALUES ('MFZA1321E1245', '1');
+
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '4', 'Amortiguador', 'Amortiguador delantero SYD', 'amortiguador.jpg','6', '899.50', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '4', 'Amortiguador GAS', 'Amortiguador delantero SYD', 'amortiguador_Gas.jpg','2', '1200.50', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '8', 'Buje delantero', 'Buje delantero SYD', 'bujes_del.jpg','8', '120.50', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '8', 'Bujes estabilizador', 'bujes o Gomas estabilizadorasdelantero SYD', 'Gomas_estabilizacion.jpg','9', '50.50', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '1', 'Bujias Platino', 'El Iridio es un metal noble que posee alta resistencia al desgaste, posibilitando la reducción del diámetro del electrodo y mejorando su encendido y durabilidad. Poseen un diámetro de electrodo central de 0,4 y 0,6mm.', 'bujiasplatino.jpeg','12', '299', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '1', 'Bujias Iridium', 'Las bujías NGK G-Power de platino son una opción de mejora económica de las bujías tradicionales. El electrodo central de alambre fino de platino provee excelente rendimiento y eficiencia en el consumo de combustible.', 'bujiasiridium.jpeg','15', '850', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '5', 'Filtro De Aceite Audi', 'KWX proporciona una amplia gama de aplicaciones en filtros para aceite, tanto para vehículos modernos como de años anteriores. Estos son ideales para lubricantes de alto rendimiento (sintéticos), y de base mineral. Cambie el filtro al menos cada 5000 kilómetros o cada 6 meses.', 'filtroaceite.jpeg','55', '144', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '5', 'Filtro de Aire sport', 'RR-2803 Se instala con parte de tubería original.', 'filtroaire.jpeg','3', '580', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '5', 'Filtro de aire Normal', 'Obtenga mejor protección para el motor y rendimiento mejorado cada 20 000 km con un filtro de aire nuevo. Un filtro de aire nuevo puede aumentar el flujo de aire, la potencia y el rendimiento del motor en general. Siga los intervalos de cambio recomendados según se indica en el manual del propietario de su vehículo.', 'filtroairenormal.jpeg','10', '250', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '5', 'Filtro de Gasolina', ' ofrece cobertura nacional y de importados excepcional en filtros de combustible - todo de una misma fuente. La línea de filtros de combustible FRAM incluye filtros de combustible enroscables convencionales y conductores de plástico para cumplir con las necesidades de prácticamente todas las configuraciones de motor.', 'filtrofram.webp','6', '650', '4');
+
+
+
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '7', 'Balatas Delanteras Audi ', 'Semi - Metálica, Juego. Incluye herrajes. El material de la balata de OE es semimetálico. Sin sensor. El reemplazo de los herrajes es sumamente recomendable con cada cambio de balatas o zapatas.', 'balatasdel.webp','26', '745', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '7', 'Balatas Tracera', 'Semi - Metálica, Incluye herrajes. Con frenos Lucas. El reemplazo de los herrajes es sumamente recomendable con cada cambio de balatas o zapatas', 'balatastraseras.webp','15', '578', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '2', 'Castrol Sintetico 5W-30 Aceite', 'El aceite es sencillamente la  del motor, sin el sus componentes se deformarían a causa del calor, es por eso que siempre debe trabajar con lubricante en buen estado y de ahí la gran importancia de cambiarlo periódicamente.', 'aceite1lit.webp','25', '350', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '2', 'Garrafa Aceite Sintético 5w40 ', 'El aceite es sencillamente la  del motor, sin el sus componentes se deformarían a causa del calor, es por eso que siempre debe trabajar con lubricante en buen estado y de ahí la gran importancia de cambiarlo periódicamente.', 'aceite5lit.webp','3', '1250', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '6', 'Jgo Rotula Inferior Izquierda Y Derecha Audi', 'EL KIT INCLUYE:- 1 ROTULA INFERIOR IZQUIERDA- 1 ROTULA INFERIOR DERECHA APLICA PARA:KIT ROTULA INFERIOR IZQUIERDA Y DERECHA AUDI A1 2011 2012', 'rotulas.webp','5', '450', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '6', 'Resortes Ag Audi ', 'La Línea Ag Confort es una línea de resortes especializada en recuperar la altura original de tu vehículo, reduciendo las molestas vibraciones y ruidos de tu suspensión con los mismos valores de carga.', 'resortes.webp','8', '1350', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '6', 'Faro Delantero Derecho Original', 'El producto que observa en las fotografías es justo el que se enviará. Si el producto trae algún detalle lo podrá ver claramente en las imágenes. Debido a que es una pieza usada puede presentar detalles de uso normal o por el manejo de la pieza, pero nada que impida su buen funcionamiento.', 'faros.webp','15', '2580', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '6', 'Rines 18 5/100', 'Rines deportivos marca AM WHEELS, estos rines son fabricados con aluminio de alta calidad, NUEVOS', 'rinaudi.webp','4', '11500', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '3', 'Batería Audi A1 Cabr13/14', '///////LA UNICA BATERÍA DEL MERCADO CON GARANTÍA DIRECTA DE 18 MESES DE REEMPLAZO SIN COSTO Y 18 MAS CON AJUSTE /////////', 'bateriaaudi.webp','9', '2511', '4');
+INSERT INTO Productos(Id_Vehiculo,Id_Cat,Nombre,Descripcion,URL_Foto,Unidades,Precio,Id_status) VALUES ('1', '3', 'Bateria Lth Agm Para Audi A1', 'LTH AGM es la mejor batería de placa plana en el mercado y cuenta con la tecnología más avanzada en baterías, lo cual se traduce en mayor poder, confianza y durabilidad. Está diseñada para durar más que las baterías convencionales y proporcionar energía a todos los accesorios eléctricos de los vehículos modernos. ', 'baterialth.webp','12', '5250', '4');
+
+
+
+
+
+
+
+
+
+
+
+
 
 insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A4',2014,"TSI 3.5 8CIL");
 insert into Modelo(Id_Marca,Modelo,Año,Motor) values (1,'A4',2015,"TSI 3.0 8CIL");
